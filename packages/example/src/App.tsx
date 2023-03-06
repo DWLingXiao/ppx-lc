9775327e7a015b992693815871053055c29a17f9
chenzexi <chenzexi@imdada.cn>
2023-03-06 17:00:42 +0800

    feat:新增类型构造器,基础json解析引擎

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import './styles/app.css';
+import React from "react";
+import "./styles/app.css";
+import { SimpleExample } from "./pages/SimpleExample";
 
 function App() {
-
-    return (
-        <div className="App">
-            hello, world.
-        </div>
-    );
+  return (
+    <div className="App">
+      <SimpleExample />
+    </div>
+  );
 }
 
 export default App;
